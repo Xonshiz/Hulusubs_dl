3b2c1802222d02263f6a17747229f4ef3db682f7
X <kanojia24.10@gmail.com>
2016-07-05 06:11:41 +0530

    Updated Setup.py for Cx_Freeze
    Using cx_Freeze to do the Binary Building job now

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python
+from cx_Freeze import setup, Executable
 
-from setuptools import setup
-
-setup(name='Distutils',
-      version='2.0',
+copyDependentFiles=True
+silent = True
+#includes = ["re",  "gzip", "encodings.cp949", "encodings.utf_8", "encodings.ascii","glob","os","sys","subprocess","selenium"]
+buildOptions = dict(
+        compressed = True,
+        includes = ["re",  "gzip", "encodings.cp949", "encodings.utf_8", "encodings.ascii","glob","os","sys","selenium","requests","bs4","time","lxml", "lxml._elementpath", "lxml.etree"],
+        include_files = ["phantomjs.exe"],
+        bin_includes = ["phantomjs.exe"]
+        )
+setup(name='Hulu Subs Downloader - Xonshiz',
+     version='3.0',
       description='Little Python Script To Download Susb from HULU.',
       author='Xonshiz',
       author_email='Xonshiz@psychoticelites.com',
-      url='https://github.com/Xonshiz/Hulu-Subs-Downloader',
-      install_requires=['lxml','bs4','requests']
-     )
+      options = dict(build_exe = buildOptions),
+      executables=[Executable('Hulu_Subs_Downloader-Xonshiz.py')],
+ ) 
