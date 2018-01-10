4e289857fa8e2dfe2fa0203fbc8c317748451f93
Xonshiz <kanojia24.10@gmail.com>
2018-01-10 16:26:12 +0530

    setup file update
    setup file update

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
-from cx_Freeze import setup, Executable
+from distutils.core import setup
 
-copyDependentFiles=True
-silent = True
-#includes = ["re",  "gzip", "encodings.cp949", "encodings.utf_8", "encodings.ascii","glob","os","sys","subprocess","selenium"]
-buildOptions = dict(
-        compressed = True,
-        includes = ["re",  "gzip", "encodings.cp949", "encodings.utf_8", "encodings.ascii","cfscrape","re","sys","selenium","urllib.request"],
-        include_files = ["ReadMe.md"],
-        bin_includes = ["ReadMe.md"]
-        )
-setup(name='Hulu Subs Downloader - Xonshiz',
-     version='4.1',
-      description='Little Python Script To Download Susb from HULU.',
-      author='Xonshiz',
-      author_email='Xonshiz@psychoticelites.com',
-      options = dict(build_exe = buildOptions),
-      executables=[Executable('HuluSubsDownloader.py')],
- ) 
+setup(
+    name = 'hulu_subs_downloader',
+    version = '4.1',
+    description = 'Little Python Script To Download Susb from HULU.',
+    author = 'Xonshiz',
+    author_email = 'Xonshiz@psychoticelites.com',
+    url = 'https://github.com/Xonshiz/Hulu-Subs-Downloader',
+    keywords = ['subtitle downloader', 'Hulu', 'hulu subtitle downloader'],
+)
