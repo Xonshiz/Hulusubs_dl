e1ed75e5f526a2ade4be52c9f83df52dc600c964
Xonshiz <kanojia24.10@gmail.com>
2021-01-04 22:13:55 +0530

    import change

--- hulusubs_dl/__main__.py
+++ hulusubs_dl/__main__.py
@@ -3,9 +3,9 @@
 
 import sys
 import os
-from hulusubs_dl import __version__
-from hulusubs_dl import cust_utils
-from hulusubs_dl import hulu_subs_dl
+from . import __version__
+from . import cust_utils
+from . import hulu_subs_dl
 
 if __name__ == "__main__":
     hulu_subs_dl.HuluSubsDl(sys.argv[1:], os.getcwd())
