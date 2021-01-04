e539ee2b01a2083715a8750db54327ec5eb6f16e
Xonshiz <kanojia24.10@gmail.com>
2021-01-04 21:42:32 +0530

    dynamic versioning in setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+
 from hulu_subs_dl import __version__
 
 readme = open('ReadMe.md').read()
@@ -6,7 +7,7 @@ history = open('Changelog.md').read()
 
 setuptools.setup(
     name="hulusubs_dl", # Replace with your own username
-    version="2021.01.05",
+    version=__version__.__version__,
     author="Xonshiz",
     author_email='xonshiz@gmail.com',
     url='https://github.com/Xonshiz/Hulu-Subs-Downloader',
